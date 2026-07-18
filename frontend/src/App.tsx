aa086e07a7ce2c3405d3a76086fc39340c76d5d6
thauu-01 <popola957th@gmail.com>
2026-07-18 13:40:18 +0700

    fix(frontend): switch to HashRouter to completely bypass Vercel server-side routing issues and fix the 404 error on refresh

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from './layouts/MainLayout';
 import SearchPage from './pages/SearchPage';
 import StatisticsPage from './pages/StatisticsPage';
@@ -7,16 +7,16 @@ import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-        <Route element={<MainLayout />}>
+        <Route path="/" element={<MainLayout />}>
           <Route index element={<SearchPage />} />
-          <Route path="/statistics" element={<StatisticsPage />} />
-          <Route path="/ranking" element={<RankingPage />} />
+          <Route path="statistics" element={<StatisticsPage />} />
+          <Route path="ranking" element={<RankingPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
